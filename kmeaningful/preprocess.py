034ec60983e3308ee6b47c3eaaffa3ef938be7bf
sbabicki <sbabicki@gmail.com>
2021-03-04 14:24:47 -0800

    Update code to pass tests

--- kmeaningful/preprocess.py
+++ kmeaningful/preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 from sklearn.pipeline import make_pipeline
 
 def preprocess(X):
@@ -28,7 +29,7 @@ def preprocess(X):
     if len(X) < 1:
         raise Exception("Please provide a dataframe X with at least one row as input")
         
-    preprocess_pipe = make_pipeline(StandardScaler())
+    preprocess_pipe = make_pipeline(SimpleImputer(), StandardScaler())
     preprocess_pipe.fit(X)
     X_processed = preprocess_pipe.transform(X)
 
