f50f6fd02c4123a5da2554661a2210c7a220b3d2
sbabicki <sbabicki@gmail.com>
2021-03-04 17:40:20 -0800

    Update documentation for input and output type

--- kmeaningful/preprocess.py
+++ kmeaningful/preprocess.py
@@ -9,12 +9,12 @@ def preprocess(X):
 
     Parameters
     ----------
-    X : DataFrame or Array
-    Unprocessed data on which to apply preprocessing steps.
+    X : Array-like of shape (n_samples, n_features)
+    Unprocessed data on which to apply preprocessing steps, can be in form of DataFrame or Array
 
     Returns
     -------
-    Numpy ndarray
+    Numpy ndarray of shape (n_samples, n_features_new)
     An array representing the data after appropriate preprocessing steps are applied.
 
     Examples
@@ -30,7 +30,6 @@ def preprocess(X):
         raise Exception("Please provide a dataframe X with at least one row as input")
         
     preprocess_pipe = make_pipeline(SimpleImputer(), StandardScaler())
-    preprocess_pipe.fit(X)
-    X_processed = preprocess_pipe.transform(X)
+    X_processed = preprocess_pipe.fit_transform(X)
 
     return X_processed
