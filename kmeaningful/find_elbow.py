56d9a279e361ffcb1c56141a3b493b115a4e23bb
Mike Lynch <lynchmi@student.ubc.ca>
2021-03-04 23:55:05 -0500

    add WIP version of find_elbow and additional test cases

--- kmeaningful/find_elbow.py
+++ kmeaningful/find_elbow.py
@@ -1,12 +1,16 @@
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+
+
 def find_elbow(X):
     """
-    This function takes in unlabeled, scaled data and performs clustering using the KMeans clustering algorithm values of K up to 10.
+    This function takes in unlabeled, scaled data and performs clustering using the KMeans clustering algorithm values of K up to the min(10, n_samples - 1).
     
     It returns the value for K which maximizes the mean silhouette scores for all clusters.
 
     Parameters
     ----------
-    X : DataFrame
+    X : numpy ndarray
     Pre-scaled data to train clustering model with.
 
     Returns
@@ -22,3 +26,17 @@ def find_elbow(X):
     >>> optimal_K = find_elbow(processed_data)
     
     """
+    # Raise error for bad input
+    if len(X) < 3:
+        raise Exception("Please provide a numpy ndarray with at least three rows")
+
+    scores = dict()
+    max_clusters = min(10, X.shape[0] - 1)
+    for K in range(2, max_clusters + 1):
+        model = KMeans(n_clusters=K)
+        model.fit(X)
+        labels = model.predict(X)
+        sil_score = silhouette_score(X, labels)
+        scores[K] = sil_score
+
+    return max(scores, key=lambda k: scores[k])
--- tests/test_find_elbow.py
+++ tests/test_find_elbow.py
@@ -1,13 +1,26 @@
 from kmeaningful.find_elbow import find_elbow
+from kmeaningful.preprocess import preprocess
 from sklearn.datasets import make_blobs
+import numpy as np
+import pytest
 
 
 def test_find_elbow():
-    # Make helper data with three distinct clusters
-    X, _ = make_blobs(n_samples=10, centers=3, n_features=2)
+
+    # empty array should raise exception
+    empty = np.ndarray(0)
+    assert pytest.raises(Exception, find_elbow, empty)
+
+    # 1 row array should raise exception
+    single_row = np.ones((1, 2))
+    assert pytest.raises(Exception, find_elbow, single_row)
+
+    # Make low dimensional helper data with three distinct clusters
+    X, _ = make_blobs(n_samples=10, centers=3, n_features=2, random_state=1234)
+    X_scaled = preprocess(X)
 
     # Use the find_elbow function to find the optimal K for the helper data
-    optimal_K = find_elbow(X)
+    optimal_K = find_elbow(X_scaled)
 
     # Make sure the the find_elbow function returns the correct type (int)
     assert isinstance(
