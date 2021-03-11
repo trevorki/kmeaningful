97a66b92bc636d2dbede2ac019fea0849b5bf90c
Hazel Jiang <jiangyh92@gmail.com>
2021-03-10 16:48:37 -0800

    fixed styling error follow flake8 for function and test file.

--- kmeaningful/show_clusters.py
+++ kmeaningful/show_clusters.py
@@ -1,12 +1,12 @@
 import pandas as pd
-import numpy as np
-from sklearn.datasets import make_blobs
 from sklearn.decomposition import PCA
 import altair as alt
 
-def show_clusters(data, clusters, centroids = None):
+
+def show_clusters(data, clusters, centroids=None):
     """
-    This function reduces a data set to 2 dimensions using principle component analysis (PCA) and colours clusters of points.
+    This function reduces a data set to 2 dimensions using
+    principle component analysis (PCA) and colours clusters of points.
     Parameters
 
 
@@ -19,7 +19,7 @@ def show_clusters(data, clusters, centroids = None):
 
     centroids: 2d array
     Coordinates of cluster centroids
-
+cd
 
     Returns
     -------
@@ -30,54 +30,55 @@ def show_clusters(data, clusters, centroids = None):
     >>> from sklearn.datasets import make_blobs
     >>> X, y = make_blobs(n_samples=10, centers=3, n_features=2)
     >>> show_clusters(X, y, centroids)
-
-    
     """
-    
+
     # Exception handling
     try:
         data = pd.DataFrame(data)
     except ValueError:
-        raise ValueError("data should be a pandas dataframe or a numpy2darray.")
-        
-    try: 
+        raise ValueError(
+            "data should be a pandas dataframe or a numpy2darray.")
+
+    try:
         clusters = pd.Series(clusters)
     except ValueError:
-        raise ValueError("clusters should be a list of numbers, a pandas series or a numpy1darray.")
-        
+        raise ValueError(
+            "clusters should be a list of numbers," +
+            "a pandas series or a numpy1darray.")
+
     if data.shape[0] != clusters.shape[0]:
-        raise ValueError("data should have the same number of rows as clusters")
+        raise ValueError(
+            "data should have the same number of rows as clusters")
 
     # Plot without centroid
     pca = PCA(n_components=2)
     principal_comp = pca.fit_transform(data)
     pca_df = pd.DataFrame(data=principal_comp, columns=["pca1", "pca2"])
     pca_df["cluster"] = pd.Series(clusters).apply(str)
-    
 
     plot = alt.Chart(pca_df).mark_point(size=20).encode(
-    alt.X('pca1'),
-    alt.Y('pca2'),
-    color='cluster')
-    
-    
-    
-    # Exception handling for plot with centroid    
-    if type(centroids) != type(None):
+        alt.X('pca1'),
+        alt.Y('pca2'),
+        color='cluster')
+
+    # Exception handling for plot with centroid
+    if not isinstance(centroids, type(None)):
         try:
             centroids = pd.DataFrame(centroids)
         except ValueError:
-            raise ValueError("centroids should be a pandas dataframe or a numpy2darray.")
-        centroids_df = pd.DataFrame(data= pca.transform(centroids), columns=["pca1", "pca2"])
-        #centroids_df = pd.DataFrame(data= np.array(centroids), columns=["pca1", "pca2"])
-        centroids_df["cluster"] = pd.Series(range(centroids.shape[0])).apply(str)
+            raise ValueError(
+                "centroids should be a pandas dataframe or a numpy2darray.")
+        centroids_df = pd.DataFrame(
+            data=pca.transform(centroids), columns=[
+                "pca1", "pca2"])
+        # centroids_df = pd.DataFrame(data= np.array(centroids),
+        #  columns=["pca1", "pca2"])
+        centroids_df["cluster"] = pd.Series(
+            range(centroids.shape[0])).apply(str)
 
-        plot_centroid = alt.Chart(centroids_df).mark_point(filled=True, shape='cross', size = 150,opacity=1).encode(
-            alt.X('pca1'),
-            alt.Y('pca2'),
-            color='cluster')
+        plot_centroid = alt.Chart(centroids_df).mark_point(
+            filled=True, shape='cross', size=150, opacity=1).encode(
+            alt.X('pca1'), alt.Y('pca2'), color='cluster')
         plot = plot + plot_centroid
-
-
 
     return plot
